3192710e5c625f66c70b27df7ed22f6d5ed6d23e
root <root@cat-emb.us-central1-a.c.atomic-lens-271105.internal>
2020-06-28 14:20:58 +0000

    update cat_emb v2

--- src/cat_emb_infer_v2.py
+++ src/cat_emb_infer_v2.py
@@ -310,6 +310,7 @@ def create_model(lr=0.002):
 
 ########################### Train Models
 #################################################################################
+'''
 for store_id in STORES_IDS:
     logger.info(f'start train {store_id}')
     
@@ -368,9 +369,11 @@ for store_id in STORES_IDS:
     # "Keep" models features for predictions
     MODEL_FEATURES = features_columns
     logger.info(f'keep features with _tmp_ : {MODEL_FEATURES}')
+'''
 
 ########################### Predict
 #################################################################################
+MODEL_FEATURES = ['item_id', 'dept_id', 'cat_id', 'release', 'sell_price', 'price_max', 'price_min', 'price_std', 'price_mean', 'price_norm', 'price_nunique', 'item_nunique', 'price_momentum', 'price_momentum_m', 'price_momentum_y', 'event_name_1', 'event_type_1', 'event_name_2', 'event_type_2', 'snap_CA', 'snap_TX', 'snap_WI', 'tm_d', 'tm_w', 'tm_m', 'tm_y', 'tm_wm', 'tm_dw', 'tm_w_end', 'enc_state_id_mean', 'enc_state_id_std', 'enc_store_id_mean', 'enc_store_id_std', 'enc_cat_id_mean', 'enc_cat_id_std', 'enc_dept_id_mean', 'enc_dept_id_std', 'enc_state_id_cat_id_mean', 'enc_state_id_cat_id_std', 'enc_state_id_dept_id_mean', 'enc_state_id_dept_id_std', 'enc_store_id_cat_id_mean', 'enc_store_id_cat_id_std', 'enc_store_id_dept_id_mean', 'enc_store_id_dept_id_std', 'enc_item_id_mean', 'enc_item_id_std', 'enc_item_id_state_id_mean', 'enc_item_id_state_id_std', 'enc_item_id_store_id_mean', 'enc_item_id_store_id_std', 'sales_lag_28', 'sales_lag_29', 'sales_lag_30', 'sales_lag_31', 'sales_lag_32', 'sales_lag_33', 'sales_lag_34', 'sales_lag_35', 'sales_lag_36', 'sales_lag_37', 'sales_lag_38', 'sales_lag_39', 'sales_lag_40', 'sales_lag_41', 'sales_lag_42', 'rolling_mean_7', 'rolling_std_7', 'rolling_mean_14', 'rolling_std_14', 'rolling_mean_30', 'rolling_std_30', 'rolling_mean_60', 'rolling_std_60', 'rolling_mean_180', 'rolling_std_180', 'rolling_mean_tmp_1_7', 'rolling_mean_tmp_1_14', 'rolling_mean_tmp_1_30', 'rolling_mean_tmp_1_60', 'rolling_mean_tmp_7_7', 'rolling_mean_tmp_7_14', 'rolling_mean_tmp_7_30', 'rolling_mean_tmp_7_60', 'rolling_mean_tmp_14_7', 'rolling_mean_tmp_14_14', 'rolling_mean_tmp_14_30', 'rolling_mean_tmp_14_60']
 
 all_preds = pd.DataFrame()
 base_test = get_base_test()
